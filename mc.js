278750d0a06a1e17b28c0f7490fcf903371bb3e2
clvrk <52180876+clvrk@users.noreply.github.com>
2020-07-28 19:20:13 -0400

    Update mc.js

--- mc.js
+++ mc.js
@@ -5,27 +5,27 @@ const config = require('./config.json');
 client.login(config.token)
 
 client.on('ready', () => {
-    let myGuild = client.guilds.cache.get('725196941050380348');
+    let myGuild = client.guilds.cache.get(''); // your server id goes here.
     let memberCount = myGuild.memberCount;
-    let memberCountChannel = myGuild.channels.cache.get('737792403146014911');
+    let memberCountChannel = myGuild.channels.cache.get(''); // your channel id goes here.
     memberCountChannel.setName('Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
 });
 
 client.on('guildMemberAdd', member => {
-    let myGuild = client.guilds.cache.get('725196941050380348');
+    let myGuild = client.guilds.cache.get(''); // your server id goes here.
     let memberCount = myGuild.memberCount;
-    let memberCountChannel = myGuild.channels.cache.get('737792403146014911');
+    let memberCountChannel = myGuild.channels.cache.get('737792403146014911'); // your channel id goes here.
     memberCountChannel.setName('Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
 });
 
 client.on('guildMemberRemove', member => {
-    let myGuild = client.guilds.cache.get('725196941050380348');
+    let myGuild = client.guilds.cache.get(''); // your server token goes here.
     let memberCount = myGuild.memberCount;
-    let memberCountChannel = myGuild.channels.get('737792403146014911');
+    let memberCountChannel = myGuild.channels.cache.get(''); // your channel id goes here.
     memberCountChannel.setName('Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
