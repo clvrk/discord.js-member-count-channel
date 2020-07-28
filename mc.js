de4e79ee3990e20eebf2bf561f38ec60af90317b
Heru Jolly <jheru711@gmail.com>
2020-07-28 18:16:03 -0400

    membercount

---
+++ mc.js
@@ -0,0 +1,28 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+const config = require('./config.json')
+const { prefix } = config;
+
+client.on('ready', () => {
+    let myGuild = client.guilds.get('725196941050380348');
+    let memberCount = myGuild.memberCount;
+    let memberCountChannel = myGuild.channels.get('737792403146014911');
+    memberCountChannel.setName('Members: ' + memberCount)
+    console.log('Member Count Has Loaded.');
+});
+
+client.on('guildMemberAdd', member => {
+    let myGuild = client.guilds.get('725196941050380348');
+    let memberCount = myGuild.memberCount;
+    let memberCountChannel = myGuild.channels.get('737792403146014911');
+    memberCountChannel.setName('Members: ' + memberCount)
+});
+
+client.on('guildMemberRemove', member => {
+    let myGuild = client.guilds.get('725196941050380348');
+    let memberCount = myGuild.memberCount;
+    let memberCountChannel = myGuild.channels.get('737792403146014911');
+    memberCountChannel.setName('Members: ' + memberCount)
+});
+
+client.login(config.token)
