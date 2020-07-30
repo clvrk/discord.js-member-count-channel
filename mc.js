ff904401e860f0708a479c74f9d8076ecb214a11
clvrk <52180876+clvrk@users.noreply.github.com>
2020-07-29 21:38:32 -0400

    Update mc.js

--- mc.js
+++ mc.js
@@ -16,7 +16,7 @@ client.on('ready', () => {
 client.on('guildMemberJoin', member => {
     let myGuild = client.guilds.cache.get(''); // your server id goes here.
     let memberCount = myGuild.memberCount;
-    let memberCountChannel = myGuild.channels.cache.get('737792403146014911'); // your channel id goes here.
+    let memberCountChannel = myGuild.channels.cache.get(''); // your channel id goes here.
     memberCountChannel.setName('Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
