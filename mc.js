bbdf4fb948da9435a526e9a351805b5d021cc347
clvrk <52180876+clvrk@users.noreply.github.com>
2020-12-22 19:32:53 -0800

    Took off one of the many un-needed events.

--- mc.js
+++ mc.js
@@ -4,15 +4,6 @@ const config = require('./config.json');
 
 client.login(config.token)
 
-client.on('ready', () => {
-    let myGuild = client.guilds.cache.get(''); // your server id goes here.
-    let memberCount = myGuild.memberCount;
-    let memberCountChannel = myGuild.channels.cache.get(''); // your channel id goes here.
-    memberCountChannel.setName('Members: ' + memberCount)
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
-});
-
 client.on('guildMemberJoin', member => {
     let myGuild = client.guilds.cache.get(''); // your server id goes here.
     let memberCount = myGuild.memberCount;
